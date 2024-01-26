11b0198bdc8997cfd4c706f153ee71adbc681b2d
darjss <darjsavid@gmail.com>
2024-01-26 08:22:20 +0800

    added styles for stopwatch and added ms

--- src/pages/time/clock.js
+++ src/pages/time/clock.js
@@ -10,7 +10,7 @@ const Clock = () => {
   };
   checkTime();
   return (
-    <div>
+    <div className="pageBackground">
       <div className="flex gap-2">
         <p>{time.toLocaleDateString()}</p>
         <p>{time.toLocaleTimeString()} </p>
--- src/pages/time/stopwatch.js
+++ src/pages/time/stopwatch.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Stopwatch = () => {
   const [clock, setClock] = useState({
+    ms: 0,
     seconds: 0,
     minutes: 0,
     hour: 0,
@@ -11,8 +12,11 @@ const Stopwatch = () => {
     const intervalId = setInterval(() => {
       setClock((prevClock) => {
         let newClock = { ...prevClock };
-        if (newClock.seconds < 59) {
-          newClock.seconds++;   
+        if (newClock.ms < 99) {
+          newClock.ms++;
+        } else if (newClock.seconds < 59) {
+          newClock.ms = 0;
+          newClock.seconds++;
         } else if (newClock.minutes < 59) {
           newClock.seconds = 0;
           newClock.minutes++;
@@ -22,7 +26,7 @@ const Stopwatch = () => {
         }
         return newClock;
       });
-    }, 1000);
+    }, 1);
     console.log(intervalId);
 
     setClock((prevClock) => ({ ...prevClock, intervalId }));
@@ -41,6 +45,7 @@ const Stopwatch = () => {
       clearInterval(prevClock.intervalId);
       console.log(prevClock.intervalId);
       return {
+        ms: 0,
         seconds: 0,
         minutes: 0,
         hour: 0,
@@ -50,20 +55,28 @@ const Stopwatch = () => {
   };
 
   const convert = (num) => {
-    let strTime;
-    num < 10 ? (strTime = "0" + num) : (strTime = num + "");
-    return strTime;
+    let strTime = num + "";
+    return strTime.padStart(2, "0");
   };
 
   return (
-    <div className="flex gap-3">
-      <p className="text-3xl">
-        {convert(clock.hour)}:{convert(clock.minutes)}:{convert(clock.seconds)}
-      </p>
-      <div className="flex gap-5">
-        <button onClick={startStopwatch}>Start</button>
+    <div className="pageBackground flexing">
+      <p className="text-6xl">Stopwatch</p>
+      <div className="bg-gray-800 w-[600px] h-[400px] flexing gap-10 rounded-xl">
+        <p className="text-6xl">
+          {convert(clock.hour)}
+          <span className="graySmall">H</span>
+          {convert(clock.minutes)}
+          <span className="graySmall">M</span>
+          {convert(clock.seconds)}
+          <span className="graySmall">S</span>
+          <span className="text-5xl">{convert(clock.ms)}</span>
+        </p>
+        <div className="flex gap-5">
         <button onClick={stopHandler}>Stop</button>
-        <button onClick={resetHandler}>Reset</button>
+          <button className="w-[100px] h-100px] bg-indigo-600 rounded-full text-2xl" onClick={startStopwatch}>Start</button>
+          <button onClick={resetHandler}>Reset</button>
+        </div>
       </div>
     </div>
   );
